fac1328ad7648274dc1564a7692d56ddfca17a40
Daniel Mejia <daniel.mix@gmail.com>
2017-08-20 13:45:48 -0500

    Manejo de clases

---
+++ ConsoleApp1/ConsoleApp1/Persona.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConociendoCSharp
+{
+    public class Persona
+    {
+        public string NombreCompleto { get; set; }
+
+        public int Edad { get; set; }
+
+        public DateTime FechaNacimiento { get; set; }
+
+        public bool? Casado { get; set; }
+
+        public Genero GeneroPersona { get; set; }
+
+        public Persona()
+        {
+        }
+
+        public Persona(string nombre, int edad)
+        {
+            NombreCompleto = nombre;
+            Edad = edad;
+
+            FechaNacimiento = DateTime.Now;
+            Casado = true;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine(NombreCompleto + " tiene " + Edad + " años");
+        }
+
+        public void MostrarTodaLaInformacion()
+        {
+            Console.WriteLine(NombreCompleto + " tiene " + Edad + " año, nació el " + FechaNacimiento.ToString("yyyy-MM-dd") + " y " + (Casado.Value ? "está casado" : "no está casado"));
+        }
+
+        public void UsandoStringFormat()
+        {
+            Console.WriteLine(String.Format("{0} tiene {1} años{100}", NombreCompleto, Edad, Environment.NewLine));
+        }
+
+        public bool EsMayorEdad()
+        {
+            return Edad >= 18;
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConociendoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1era forma de instancias
+            Persona persona1 = new Persona();
+            persona1.NombreCompleto = "Jairo";
+            persona1.Edad = 37;
+            persona1.FechaNacimiento = new DateTime(1980, 8, 13);
+            persona1.Casado = true;
+            persona1.GeneroPersona = Genero.Masculino;
+
+            //2da forma de instanciar
+            Persona persona2 = new Persona
+            {
+                NombreCompleto = "Lina",
+                Edad = 37,
+                FechaNacimiento = new DateTime(1979, 12, 29),
+                Casado = true,
+                GeneroPersona = Genero.Femenino
+            };
+
+            // 3re forma de instanciar usando el constructor que tiene los 2 parametros de entrada
+            Persona persona3 = new Persona("Mariana", 7);
+            persona3.Casado = false;
+            persona3.GeneroPersona = Genero.Femenino;
+
+            Console.WriteLine(persona3.GeneroPersona);
+
+            //Console.WriteLine(persona1.NombreCompleto);
+            //Console.WriteLine(persona2.NombreCompleto);
+            //Console.WriteLine(persona3.NombreCompleto);
+
+            //persona1.MostrarInformacion();
+            //persona2.MostrarInformacion();
+            //persona3.MostrarInformacion();
+
+            persona1.MostrarTodaLaInformacion();
+            persona2.MostrarTodaLaInformacion();
+            persona3.MostrarTodaLaInformacion();
+
+
+
+            int? valor = null;
+
+            if (!valor.HasValue)
+            {
+                valor = 10;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
